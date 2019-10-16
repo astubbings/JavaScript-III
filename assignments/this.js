2681f3eb87d9cb1820ac1494f34ce006a05ecf48
astubbings <53932400+astubbings@users.noreply.github.com>
2019-10-16 14:46:44 -0600

    intial commit to setup pull

--- assignments/this.js
+++ assignments/this.js
@@ -1,8 +1,8 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
+* 1. Window or Global object binding - when none of the other ruels apply 'this' defaults to the window Object
+* 2. most of the time we will use Implicit Binding which means 'this' refers to the object it is contained in (best way i can think about it.)
 * 3. 
 * 4. 
 *
